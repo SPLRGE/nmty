e34e8ab3f5b0169c23f189d4c0fa4aa43598b4ba
Julien <61233505+dev-cetus@users.noreply.github.com>
2024-03-21 21:30:00 +0000

    feat: beautiful cards

--- pages/team.vue
+++ pages/team.vue
@@ -18,13 +18,11 @@ const { data: team } = await useAsyncData('team', () => {
 <template>
   <div class="py-12 text-center">
     <h1 class="font-changa-one text-4xl md:text-6xl">Notre équipe</h1>
-    <div class="mx-auto my-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-      <div v-for="member in team" :key="member.id" class="rounded-md bg-gray-100 p-4 shadow-md dark:bg-zinc-800">
-        <NuxtImg
-          :src="getThumbnail(member.avatar)"
-          :alt="member.name"
-          class="mx-auto h-[100px] w-[100px] rounded-full md:h-[150px] md:w-[150px] lg:h-[200px] lg:w-[200px]"
-        />
+    <div class="mx-auto my-12 flex flex-wrap justify-center gap-4">
+      <div v-for="member in team" :key="member.id"
+        class="rounded-md group bg-gray-100 p-10 shadow-md dark:bg-zinc-800 hover:cursor-pointer">
+        <NuxtImg :src="getThumbnail(member.avatar)" :alt="member.name"
+          class="mx-auto h-[100px] w-[100px] rounded-full md:h-[150px] md:w-[150px] lg:h-[200px] lg:w-[200px] group-hover:scale-105 transition-transform" />
         <p class="pt-2">{{ member.name }}</p>
       </div>
     </div>
